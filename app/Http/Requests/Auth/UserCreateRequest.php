3622aec7600af369f52661de9c0664b37f1718c5
Hylton Walters <hylton@inqaku.com>
2026-08-16 15:04:46 +0200

    refactor: use geolocation service for user creation

--- app/Http/Requests/Auth/UserCreateRequest.php
+++ app/Http/Requests/Auth/UserCreateRequest.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Services\IpGeolocationService;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Http;
 
 class UserCreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
-     * @return bool
      */
     public function authorize(): bool
     {
@@ -19,8 +17,6 @@ class UserCreateRequest extends FormRequest
 
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array[]
      */
     public function rules(): array
     {
@@ -35,34 +31,17 @@ class UserCreateRequest extends FormRequest
     }
 
     /**
-     * Create the user location before creating the user.
-     *
-     * @return array
+     * Prepare validated user data and initial location metadata.
      */
     public function createUser(): array
     {
         $userData = $this->validated();
         $userData['password'] = bcrypt($userData['password']);
 
-        // Retrieve the user's IP address and user agent
         $ipAddress = $this->ip();
         $userAgent = $this->userAgent();
+        $city = app(IpGeolocationService::class)->cityFor($ipAddress) ?? 'Unknown';
 
-        // Get location data using IP address from API
-        $locationData = $this->getLocationData($ipAddress);
-
-        // Extract city name from location data
-        $city = $locationData['city'] ?? null;
-
-        // If city data is not available or API request fails,
-        // like in my case it can be one of the following reasons:
-        // private range, reserved range, invalid query
-        // we then generate a fake city name
-        if (!$city) {
-            $city = fake()->city;
-        }
-
-        // Create the user location
         $userLocationData = [
             'ip_address' => $ipAddress,
             'user_agent' => $userAgent,
@@ -70,27 +49,8 @@ class UserCreateRequest extends FormRequest
             'login_at' => now(),
         ];
 
-        // Get roles from the request data
         $roles = $this->input('roles');
 
-        // Return both user data and user location data, and role ID
         return compact('userData', 'userLocationData', 'roles');
-    }
-
-    /**
-     * Get location data using IP address from API.
-     *
-     * @param string $ipAddress
-     * @return array
-     */
-    private function getLocationData(string $ipAddress): array
-    {
-        $response = Http::get('http://ip-api.com/json/' . $ipAddress . '?fields=49168');
-
-        if ($response->successful()) {
-            return $response->json();
-        }
-
-        return [];
     }
 }
