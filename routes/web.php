513092eb4422e1df5b7e61d06a9ec7accc8d19c8
Hylton Walters <hylton@inqaku.com>
2026-08-16 14:42:42 +0200

    fix: route manager dashboard to manager controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\Manager\UserController as ManagerUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,11 +26,11 @@ Route::get('/dashboard', function () {
 Route::middleware(['auth'])->group(function () {
 
     Route::prefix('manager')->middleware(['manager'])->group(function () {
-        Route::get('dashboard', [UserController::class, 'dashboard'])->name('manager.dashboard');
+        Route::get('dashboard', [ManagerUserController::class, 'dashboard'])->name('manager.dashboard');
     });
 
     Route::prefix('admin')->middleware(['admin'])->group(function () {
-        Route::resource('users', UserController::class);
+        Route::resource('users', AdminUserController::class);
     });
 });
 
