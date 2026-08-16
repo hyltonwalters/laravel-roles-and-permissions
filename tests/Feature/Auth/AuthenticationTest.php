325d70f775ac9b615d53f284ded6701fadc71c40
Hylton Walters <hylton@inqaku.com>
2026-08-16 15:05:07 +0200

    test: cover new-device login tracking

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Mail\NewDeviceLoginNotification;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
@@ -20,6 +23,11 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
+        Http::fake([
+            'https://ipwho.is/*' => Http::response(['success' => true, 'city' => 'Cape Town'], 200),
+        ]);
+        Mail::fake();
+
         $user = User::factory()->create();
 
         $response = $this->post('/login', [
@@ -29,10 +37,41 @@ class AuthenticationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+        $this->assertDatabaseHas('user_locations', [
+            'user_id' => $user->id,
+            'location' => 'Cape Town',
+        ]);
+        Mail::assertSent(NewDeviceLoginNotification::class, 1);
+    }
+
+    public function test_login_tracking_falls_back_deterministically_when_geolocation_fails(): void
+    {
+        Http::fake([
+            'https://ipwho.is/*' => Http::response([], 503),
+        ]);
+        Mail::fake();
+
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+        $this->assertDatabaseHas('user_locations', [
+            'user_id' => $user->id,
+            'location' => 'Unknown',
+        ]);
+        Mail::assertSent(NewDeviceLoginNotification::class, 1);
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
+        Mail::fake();
+        Http::fake();
+
         $user = User::factory()->create();
 
         $this->post('/login', [
@@ -41,6 +80,7 @@ class AuthenticationTest extends TestCase
         ]);
 
         $this->assertGuest();
+        Mail::assertNothingSent();
     }
 
     public function test_users_can_logout(): void
